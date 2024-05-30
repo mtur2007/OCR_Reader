87683cc44e93fc55bb5b074b90a9a5ef2dcca0f3
松浦　憲真 <matsuurakenshin@matsuurakenshinnoMacBook-Air.local>
2024-05-30 18:53:59 +0900

    サーチ用データ作成と識字プログラムで分ける

--- txtreader_Mk-II/SET_search_datas.py
+++ txtreader_Mk-II/SET_search_datas.py
@@ -4,27 +4,6 @@ import numpy as np
 
 
 #-----------------------------------------------------------------------------------------------------------
-filename = "/Users/matsuurakenshin/WorkSpace/development/txtreader/txtreader_Mk-II/image_file_name.txt"
-imageslist = []
-import os
-if os.path.exists(filename) == False:
-    with open("image_file_name.txt","w") as f:
-        f.write("image_file_name")
-
-imagenamefile = "/Users/matsuurakenshin/WorkSpace/development/txtreader/txtreader_Mk-II/image_file_name.txt"
-if os.path.exists(imagenamefile) == True:
-    with open(imagenamefile,"r") as f:
-        for line in f:
-            line = line.strip()
-            if line != "image_file_name":
-                if os.path.exists(line) == True:
-                    imageslist.append(str(line.strip()))
-                else:
-                    print(f'image_file : "{line}" は存在しません。')
-
-
-#-----------------------------------------------------------------------------------------------------------
-
 
 def removal_background(color_image,RGB,kyoyou): #写真のNumPy配列を渡すと戻り値として背景を１とし、それ以外を0に置き換えた配列が戻ってくる。
     background_color = np.array(RGB)
@@ -315,23 +294,21 @@ def print_textdatas(dataslist,writefilename):
 
 #-----------------------------------------------------------------------------------------------------------
 
-for imagename in imageslist:
 
-    if imageslist[0] == "/Users/matsuurakenshin/WorkSpace/development/txtreader/txtreader_Mk-II/textdata.jpeg":
-        dataslist = image_removal_background(imagename,[36,36,36],180)
-    else:
-        dataslist = image_removal_background(imagename,[36,36,36],60)
-        
-    dataslist = seach_txtposition(dataslist,100)
-    dataslist = txtdatas_insert(dataslist)
+dataimage = "/Users/matsuurakenshin/WorkSpace/development/txtreader/txtreader_Mk-II/textdata.jpeg"
 
-    print_textdatas(dataslist,"Alltextimages.text")
-    txtimage = dataslist["Alltxtimages"]
-    txtdata = dataslist["Alltxtdatas"]
-    #plt.imshow(readtxt_imshow(dataslist))
-    #plt.imshow(cv2.resize(txtimage[0][0],dsize=(10,30)))
-    #print(txtdata[0][1])
-    print_textdatas(dataslist,"textdataslist_printfile.txt")
+dataslist = image_removal_background(dataimage,[36,36,36],180)
+    
+dataslist = seach_txtposition(dataslist,100)
+dataslist = txtdatas_insert(dataslist)
+
+print_textdatas(dataslist,"Alltextimages.text")
+txtimage = dataslist["Alltxtimages"]
+txtdata = dataslist["Alltxtdatas"]
+#plt.imshow(readtxt_imshow(dataslist))
+#plt.imshow(cv2.resize(txtimage[0][0],dsize=(10,30)))
+#print(txtdata[0][1])
+print_textdatas(dataslist,"textdataslist_printfile.txt")
 
 
 #===========================================================================================================
@@ -344,91 +321,6 @@ import pickle
 with open('data.pickle', mode='wb') as fo:
   pickle.dump(txtimage, fo)
 """
-
-### pickleで保存したファイルを読み込み
-with open('/Users/matsuurakenshin/WorkSpace/development/txtreader/txtreader_Mk-II/data.pickle', mode='br') as fi:
-  txtimage = pickle.load(fi)
-
-
-#===========================================================================================================
-
-
-def Myint(num): #数値の int部分を確実に表示させる様にする自作関数
-    for line in range(len(str(num))):
-        if str(num)[line] == ".":
-            return int(str(num)[:line])
-    return num
-
-
-#数値の整列を行う関数
-
-def SET_numbertxt(numberslist,mode):
-    if mode == 0:
-        Max = 0
-        len_list = []
-
-        for line in numberslist:
-            len_list.append(len(line))
-            if len(line) > Max:
-                Max = len(line)
-        
-        New_list = []
-        Line_list = []
-        for nouse in range(Max):
-            New_list.append("")
-
-        for line in numberslist:
-            Set_list = copy.deepcopy(New_list)
-
-            for number in range(len(line)):
-                Set_list[number] = line[number]
-            
-            Line_list.append(Set_list)
-        
-        Line_list = np.array(Line_list)
-
-        search_list = []
-        for num in range(np.shape(Line_list)[1]):
-            search_list.append(Line_list[:,num])
-
-        search_list = np.array(SET_numbertxt(search_list,1))
-        Line_list = []
-
-        for num in range(np.shape(search_list)[1]):
-            Line_list.append(search_list[:,num])
-
-        returndata = []
-        for line in range (np.shape(Line_list)[0]):
-            cut = len_list[line]
-            returndata.append(Line_list[line][:cut])
-
-        return returndata
-
-    
-    elif mode == 1:
-
-        for line in range(len(numberslist)):
-            Maxintlen,Maxfloatlen = 0,0
-
-            for num in numberslist[line]:
-
-                if len(str(Myint(num))) > Maxintlen:
-                    Maxintlen = len(str(Myint(num)))
-
-                if len(str(num)) - len(str(Myint(num))) > Maxfloatlen:
-                    Maxfloatlen = len(str(num)) - len(str(Myint(num)))
-
-            Maxlen = Maxintlen + Maxfloatlen
-
-            for nowread in range(len(numberslist[line])):
-                num = numberslist[line][nowread]
-                Air0 = Maxintlen - len(str(Myint(num)))
-                Air1 = Maxlen - (Air0 + len(str(num)))
-
-                numberslist[line][nowread] = (Air0 * " ") + str(num) + (Air1 * " ")
-
-        return numberslist#[:-1]
-
 
 #===========================================================================================================
 
