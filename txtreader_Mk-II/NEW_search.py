15fd136ab7c554dc747dc9d41822278b5b941aa8
松浦　憲真 <2007helloworld@gmail.com>
2024-06-09 11:09:43 +0900

    斜辺調査プログラムの試験的な関数化

--- txtreader_Mk-II/NEW_search.py
+++ txtreader_Mk-II/NEW_search.py
@@ -4,11 +4,15 @@ import numpy as np
 
 import pickle
 
-# seach_textdatas
-with open('/Users/matsuurakenshin/WorkSpace/development/txtreader/txtreader_Mk-II/Save_retest.pickle', mode='br') as fi:
-    seach_textdatas,retest = pickle.load(fi)
+### pickleで保存したファイルを読み込み
+with open('/Users/matsuurakenshin/WorkSpace/development/sample_txtdata.pickle', mode='br') as fi:
+    dataslist,insert_txtdatas,seach_textdatas = pickle.load(fi)
 
-seach_textdatas
+def keys_print():
+    print(f"\ndataslist_keys:")
+    for info_key in dataslist:
+        print(f">> {info_key}")
+    print()
 
 #------------------------------------------------------------------------------------------------------------------------
 
@@ -148,3 +152,29 @@ print(f"MIN: {np.min(sa)}")
 
 print()
 
+def removal_background(color_image,RGB,kyoyou): #写真のNumPy配列を渡すと戻り値として背景を１とし、それ以外を0に置き換えた配列が戻ってくる。
+    background_color = np.array(RGB)
+
+    code0list = np.ones((color_image.shape[0]*color_image.shape[1],1), dtype='i1') #一旦0で埋める
+
+    sa = np.abs(background_color - color_image)
+    sa = sa.reshape(color_image.shape[0]*color_image.shape[1],3) #1ピクセル毎に背景色RGBと写真のRGBの差の絶対値の集合値を算出する為、配列を縦一列、列数３にする。
+    text = np.array(np.where((np.sum(sa,axis=1)) > kyoyou)[0]) #背景色判定の許容値を超えた場合文字判定。
+
+    code0list[text, 0] = np.array(0) #code0listに文字判定の場所を再代入。
+    code0list = code0list.reshape(color_image.shape[0],color_image.shape[1]) #写真の比率にリサイズ。
+
+    return code0list
+
+def NEW_search(txtimage,search_txtdata,dataslist):
+    rgb = dataslist["background_color"]
+    kyoyoucolor = dataslist["kyoyou"]
+    shape = np.shape(sample_txtdata)
+
+    set_image = removal_background(cv2.resize(txtimage,dsize=(shape[1],shape[0])),rgb,kyoyoucolor)
+
+
+keys_print()
+
+Alltxtdatas = dataslist["Alltxtdatas"]
+#NEW_search()
